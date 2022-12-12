eb772a37c34eb9bff714c3e30cb74346ad9995c0
Saad24 <saad.24.ahm@gmail.com>
2022-12-12 16:51:45 +0000

    movie controller update

--- src/main/java/com/example/BNTA_Backend_Project/controllers/MovieController.java
+++ src/main/java/com/example/BNTA_Backend_Project/controllers/MovieController.java
@@ -36,14 +36,20 @@ public class MovieController {
         return new ResponseEntity<>(movieRepository.save(movie), HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/{id}")
+    @PatchMapping (value = "/{id}")
     public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie, @PathVariable Long id){
-        return null;
+        Movie movieToUpdate = movieRepository.findById(id).get();
+        movieToUpdate.setTitle(movie.getTitle());
+        movieToUpdate.setGenre(movie.getGenre());
+        movieToUpdate.setDuration(movie.getDuration()); 
+        movieRepository.save(movieToUpdate);
+        return new ResponseEntity<>(movieToUpdate, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Long> deleteMovie(@PathVariable Long id){
-        return null;
+        movieRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
 }

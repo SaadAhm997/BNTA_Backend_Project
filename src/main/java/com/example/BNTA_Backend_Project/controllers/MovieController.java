f984c56a808271b864e4a4c460aa35bd64ca66aa
Saad24 <saad.24.ahm@gmail.com>
2022-12-12 14:33:25 +0000

    started writing movie controller

--- src/main/java/com/example/BNTA_Backend_Project/controllers/MovieController.java
+++ src/main/java/com/example/BNTA_Backend_Project/controllers/MovieController.java
@@ -2,35 +2,38 @@ package com.example.BNTA_Backend_Project.controllers;
 
 import com.example.BNTA_Backend_Project.models.Movie;
 import com.example.BNTA_Backend_Project.models.Review;
+import com.example.BNTA_Backend_Project.repositories.MovieRepository;
 import com.example.BNTA_Backend_Project.services.MovieService;
 import com.example.BNTA_Backend_Project.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
 
     @Autowired
-    MovieService movieService;
+    MovieRepository movieRepository;
 
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies(){
-        return null;
+        return new ResponseEntity<>(movieRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<List<Movie>> getMovieById(@PathVariable Long id){
-        return null;
+    public ResponseEntity<Optional<Movie>> getMovieById(@PathVariable Long id){
+        return new ResponseEntity<>(movieRepository.findById(id), HttpStatus.OK);
     }
 
     //TODO check whether we need "List<Movie>"
     @PostMapping
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie){
-        return null;
+        return new ResponseEntity<>(movieRepository.save(movie), HttpStatus.CREATED);
     }
 
     @PutMapping(value = "/{id}")

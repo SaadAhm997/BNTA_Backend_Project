df272178dab87d96953da92eb336fdd0ed4592d3
Radhika <radhikarathi308@gmail.com>
2022-12-12 17:14:03 +0000

    review controller

--- src/main/java/com/example/BNTA_Backend_Project/controllers/ReviewController.java
+++ src/main/java/com/example/BNTA_Backend_Project/controllers/ReviewController.java
@@ -1,45 +1,56 @@
 package com.example.BNTA_Backend_Project.controllers;
 
+import com.example.BNTA_Backend_Project.models.Movie;
 import com.example.BNTA_Backend_Project.models.Review;
+import com.example.BNTA_Backend_Project.repositories.MovieRepository;
+import com.example.BNTA_Backend_Project.repositories.ReviewRepository;
 import com.example.BNTA_Backend_Project.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/reviews")
 public class ReviewController {
 
     @Autowired
-    ReviewService reviewService;
+    ReviewRepository reviewRepository;
 
     @GetMapping
     public ResponseEntity<List<Review>> getAllReviews(){
-        return null;
+        return new ResponseEntity<>(reviewRepository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<List<Review>> getReviewById(@PathVariable Long id){
-        return null;
+    public ResponseEntity<Optional<Review>> getMovieById(@PathVariable Long id){
+        return new ResponseEntity<>(reviewRepository.findById(id), HttpStatus.OK);
     }
 
-    //TODO check whether we need "List<Review>"
+    //TODO check whether we need "List<Movie>"
+
+    //TODO figure out post mapping for reviews
     @PostMapping
     public ResponseEntity<Review> addReview(@RequestBody Review review){
-        return null;
+        return new ResponseEntity<>(reviewRepository.save(review), HttpStatus.CREATED);
     }
 
-    @PutMapping(value = "/{id}")
+    @PatchMapping (value = "/{id}")
     public ResponseEntity<Review> updateReview(@RequestBody Review review, @PathVariable Long id){
-        return null;
+        Review reviewToUpdate = reviewRepository.findById(id).get();
+        reviewToUpdate.setReviewBody(review.getReviewBody());
+        reviewToUpdate.setRating(review.getRating());
+        reviewRepository.save(review);
+        return new ResponseEntity<>(reviewToUpdate, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Long> deleteReview(@PathVariable Long id){
-        return null;
+        reviewRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
-
 
 }

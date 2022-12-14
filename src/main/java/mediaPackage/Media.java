b3ccc2954edc9bf49c7ec0d38e92170ecd3f6a8b
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2022-12-14 09:03:14 -0500

    tryna figure out polymorphism

---
+++ src/main/java/mediaPackage/Media.java
@@ -0,0 +1,19 @@
+package mediaPackage;
+
+public final class Media implements MediaInterface{
+        private String title;
+
+        public Media(String title){
+            this.title = title;
+        }
+
+    @Override
+    public String setTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public String getTitle() {
+        return null;
+    }
+}
---
+++ src/main/java/mediaPackage/MediaInterface.java
@@ -0,0 +1,8 @@
+package mediaPackage;
+
+public interface MediaInterface {
+
+    public String setTitle(String title);
+    public String getTitle();
+
+}
